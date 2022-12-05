2fa68626d69b7109e96744c7b80b6f4c2e75a898
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2022-12-05 15:32:24 +0530

    16

--- PerfectNumber.cpp
+++ PerfectNumber.cpp
@@ -1,9 +1,30 @@
-#include<iostream>
+// Write a program to identify if the number is Perfect number or not
+// E.g. Let number is 28, factors of 28 are 1,2,4,7,14. Now the sum of all these factors are 28 itself.
+#include <iostream>
+
 using namespace std;
 
-int main(){
-    int a;
-    int b;
-    cout<<"Enter a number:";
-    cin>>a;
+int main()
+{
+    int a, i, sum = 0;
+    cout << "Enter a number:\n";
+    cin >> a;
+    cout << "Factors are :";
+    for (i = 1; i < a; i++)
+    {
+        if (a % i == 0)
+        {
+            cout << i << " ";
+            sum = sum + i;
+        }
+    }
+    if(sum == a)
+    {
+        cout<<"\nIt is a Perfect Number";
+    }
+    else
+    {
+        cout<<"\nIt is not a Perfect Number";
+    }
+    return 0;
 }
