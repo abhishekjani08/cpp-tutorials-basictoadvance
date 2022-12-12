1f361fc093d766c677eeb66732b9ec4674b6c61c
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2022-12-12 22:06:40 +0530

    20

---
+++ Palindrome.cpp
@@ -0,0 +1,27 @@
+
+// Write a program to identify if the number is Palindrome or not
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, palin=0, rev=0,n;
+    cout << "Enter a number:\n";
+    cin >> a;
+    n=a;
+    do
+    {
+        palin = a % 10;
+        rev = (rev * 10) + palin;
+        a = a / 10;
+    } while (a != 0);
+    cout<<rev;
+    if (n == rev)
+    {
+        cout << "It is a palindrome number";
+    }
+    else
+    {
+        cout << "It is not a palindrome number";
+    }
+    return 0;
+}
